5a1ed3ff3db0ae764b186a80b33881d0b3a9503e
lutskermari <luckermaria9@gmail.com>
2026-08-10 20:13:18 +0300

    fix

--- mysite/celery_app.py
+++ mysite/celery_app.py
@@ -1,8 +1,9 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
 
 app = Celery("mysite")
 app.config_from_object("django.conf:settings", namespace="CELERY")
@@ -11,10 +12,10 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     "clear-expired-sessions-every-day": {
         "task": "bookshop.tasks.clear_expired_sessions",
-        "schedule": crontab(hour=0, minute=0),  # Щодня о півночі
+        "schedule": crontab(hour=0, minute=0),
     },
     "generate-daily-report": {
         "task": "bookshop.tasks.generate_daily_sales_report",
-        "schedule": crontab(hour=23, minute=50),  # Щодня о 23:50
+        "schedule": crontab(hour=23, minute=50),
     },
 }
